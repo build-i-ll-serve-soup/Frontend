bdf192023a5e85f79cc1e6c8203325bd07ad799d
kly001 <karenly1961@gmail.com>
2019-08-21 17:21:24 -0400

    Setup Login & Signup components

---
+++ i-ll-serve-soup/src/components/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Login = props => {
+    return (
+        <h2>This is the Login component</h2>
+    )
+}
+export default Login
---
+++ i-ll-serve-soup/src/components/Signup.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Signup = props => {
+    return (
+        <h2>This is the Signup component</h2>
+    )
+}
+export default 
+Signup
