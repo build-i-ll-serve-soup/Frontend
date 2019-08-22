544d396aab444064239bc68998ab4a84977e00bf
Adam Hinckley <adamhinckley@mac.com>
2019-08-22 10:37:50 -0600

    New user can successfully register

--- i-ll-serve-soup/src/components/Signup.js
+++ i-ll-serve-soup/src/components/Signup.js
@@ -1,63 +1,111 @@
 import React, { useState } from "react"; // Import useEffect
 import { Form, Field, withFormik } from "formik";
-import '../App.css';
-// Import Axios
-const Signup = ({values, status}) => {
-    //
-    // Example of handling state change using response
-    //
-    // const [users, setUsers] = useState([]);
+import "../App.css";
+import axios from "axios";
 
-    // useEffect(() => {
-    //   if (status) {
-    //     setUsers(users => [...users, status]);
-    //     console.log(users);
-    //   }
-    // }, [status]);
+const deployedUrl = "https://bw-ill-serve-soup.herokuapp.com";
+const localUrl = "http://localhost:5000";
+
+const Signup = ({ values, status }) => {
+  //
+  // Example of handling state change using response
+  //
+  const [user, setUser] = useState({});
+
+  // useEffect(() => {
+  //   if (status) {
+  //     setUsers(users => [...users, status]);
+  //     console.log(users);
+  //   }
+  // }, [status]);
+
+  const changeHandler = e => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  console.log(user);
+
+  const submitHandler = e => {
+    e.preventDefault();
+    axios
+      .post(`${deployedUrl}/api/users/register`, user)
+      .then(res => {
+        console.log(res.status);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="onboard-form">
-      <Form className="form">
-            <Field component="input" type="text" name="name" placeholder="Name" / >
-            <Field component="input" type="password" name="password" placeholder="Password" / >
-            <Field component="input" type="email" name="email" placeholder="Email" / >
-            <Field component="input" type="text" name="role" placeholder="Role" / >
-            <button className="form-button" type="submit">Submit</button>
+      <Form className="form" onSubmit={submitHandler}>
+        <Field
+          component="input"
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={changeHandler}
+        />
+        <Field
+          component="input"
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={changeHandler}
+        />
+        <Field
+          component="input"
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={changeHandler}
+        />
+        <Field
+          component="input"
+          type="text"
+          name="role"
+          placeholder="Role"
+          onChange={changeHandler}
+        />
+        <button className="form-button" type="submit">
+          Submit
+        </button>
       </Form>
     </div>
   );
 };
 
 const formikHOC = withFormik({
-    mapPropsToValues({ name, password, email, role }){
-        return {
-            name: name,
-            password: password,
-            email: email,
-            role: role
-        }
-    },
-    handleSubmit(values, { setStatus, resetForm }){
-        
-        console.log(values);
-        resetForm();
+  mapPropsToValues({ name, password, email, role }) {
+    return {
+      name: name,
+      password: password,
+      email: email,
+      role: role
+    };
+  },
+  handleSubmit(values, { setStatus, resetForm }) {
+    console.log(values);
+    resetForm();
 
-        //
-        // Example of using formm data in axios request...
-        //
-        // handleSubmit(values, { setStatus, resetForm }) {
-        //     axios
-        //       .post("https://reqres.in/api/users", values)
-        //       .then(response => {
-        //         // console.log(response)
-        //         setStatus(response.data);
-        //         resetForm();
-        //       })
-        //       .catch(error => {
-        //         console.log("Submission error...", error);
-        //       });
-        //   }
-    }
-})
+    //
+    // Example of using formm data in axios request...
+    //
+    // handleSubmit(values, { setStatus, resetForm }) {
+    //     axios
+    //       .post("https://reqres.in/api/users", values)
+    //       .then(response => {
+    //         // console.log(response)
+    //         setStatus(response.data);
+    //         resetForm();
+    //       })
+    //       .catch(error => {
+    //         console.log("Submission error...", error);
+    //       });
+    //   }
+  }
+});
 
 export default formikHOC(Signup);
