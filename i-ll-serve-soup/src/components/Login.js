5e2a02ca478e7a2fe38f905802369e7671b4ec73
kly001 <karenly1961@gmail.com>
2019-08-22 22:12:11 -0400

    Changes to signup & login displaying in console

--- i-ll-serve-soup/src/components/Login.js
+++ i-ll-serve-soup/src/components/Login.js
@@ -1,72 +1,6 @@
-// import React, {useState} from "react";
-// import { Link } from "react-router-dom";
-
-// const Login = props => {
-
-//     const [user, setUser] = useState("")
-
-//     const changeHandler = event => {
-//         // console.log(event)
-//         setUser({...user, [event.target.name]: event.target.value})
-//     }
-
-//     const submitForm = event => {
-//         console.log(event)
-//         event.preventDefault();
-//         // const newUser = {
-//         //     ...user,
-//         //     id:Date.now()
-//         // }
-//         // props.addNewUser(user);
-//         // console.log(newUser)
-//     }
-
-//     return(
-//         <div>
-//         <form onSubmit = {submitForm}>
-//             <label htmlFor = "name">  User Name </label>
-//             <input
-//                 type="text"
-//                 name="name"
-//                 placeholder="Type Name Here"
-//                 value={user.name}
-//                 onChange={changeHandler}
-//                 />
-//                  <label htmlFor = "password">  User Password </label>
-//                  <input
-//                 type="text"
-//                 name="password"
-//                 placeholder="Type Password Here"
-//                 value={user.name}
-//                 onChange={changeHandler}
-//                 />
-//              <label htmlFor = "email">  User Email </label>    
-//                  <input
-//                 type="email"
-//                 name="email"
-//                 placeholder="Type Email Here"
-//                 value={user.email}
-//                 onChange={changeHandler}
-//                 />
-//              <label htmlFor = "role">  User Role </label>
-//                  <input
-//                 type="text"
-//                 name="role"
-//                 placeholder="Type Role Here"
-//                 value={user.role}
-//                 onChange={changeHandler}
-//                 />
-//             <button type="submit">Submit Login</button>
-//         </form>
-//         <p>Not a  Member? Sign up   <Link to="/signup">here</Link>.</p>
-//     </div>
-//     )
-// }
-// export default Login
-
 import React, { useState } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const deployedUrl = "https://bw-ill-serve-soup.herokuapp.com";
 const localUrl = "http://localhost:5000";
@@ -106,7 +40,6 @@ const Login = props => {
 
   return (
     <div>
-      
       <form onSubmit={submitForm}>
         <label htmlFor="name"> User Email </label>
         <input
--- i-ll-serve-soup/src/components/Signup.js
+++ i-ll-serve-soup/src/components/Signup.js
@@ -1,70 +1,3 @@
-// import React, {useState} from "react";
-// import { Link } from "react-router-dom";
-
-// const Signup = props => {
-
-//     const [newUser, setNewUser] = useState("")
-
-//     const changeHandler = event => {
-//         // console.log(event)
-//         setNewUser({...newUser, [event.target.name]: event.target.value})
-
-//     }
-
-//     const submitForm = event => {
-//         console.log(event)
-//         event.preventDefault();
-//         // const newUser = {
-//         //     ...user,
-//         //     id:Date.now()
-//         // }
-//         props.addNewUser(newUser);
-//         console.log(newUser)
-//     }
-
-//     return(
-//         <div>
-//         <form onSubmit = {submitForm}>
-//             <label htmlFor = "name">  New User Name </label>
-//             <input
-//                 type="text"
-//                 name="name"
-//                 placeholder="Type Name Here"
-//                 value={newUser.name}
-//                 onChange={changeHandler}
-//                 />
-//                  <label htmlFor = "password">  New User Password </label>
-//                  <input
-//                 type="text"
-//                 name="password"
-//                 placeholder="Type Password Here"
-//                 value={newUser.name}
-//                 onChange={changeHandler}
-//                 />
-//              <label htmlFor = "email">  New User Email </label>    
-//                  <input
-//                 type="email"
-//                 name="email"
-//                 placeholder="Type Email Here"
-//                 value={newUser.email}
-//                 onChange={changeHandler}
-//                 />
-//              <label htmlFor = "role">  New User Role </label>
-//                  <input
-//                 type="text"
-//                 name="role"
-//                 placeholder="Type Role Here"
-//                 value={newUser.role}
-//                 onChange={changeHandler}
-//                 />
-//             <button type="submit">Submit Signup</button>
-//         </form>
-//         <p>Already a Member? LogIn <Link to="/login">here</Link>.</p>
-//         </div>
-//     )
-// }
-// export default Signup
-
 import React, { useState } from "react"; // Import useEffect
 import { Form, Field, withFormik } from "formik";
 import { Link } from "react-router-dom";
@@ -108,9 +41,7 @@ const Signup = ({ values, status }) => {
 
   return (
     <div className="onboard-form">
-       
       <Form className="form" onSubmit={submitHandler}>
-      <label htmlFor="name"> New User Email </label>
         <Field
           component="input"
           type="text"
@@ -118,7 +49,6 @@ const Signup = ({ values, status }) => {
           placeholder="Name"
           onChange={changeHandler}
         />
-         <label htmlFor="password"> New User Password </label>
         <Field
           component="input"
           type="password"
@@ -126,7 +56,6 @@ const Signup = ({ values, status }) => {
           placeholder="Password"
           onChange={changeHandler}
         />
-         <label htmlFor = "email">  New User Email </label>  
         <Field
           component="input"
           type="email"
@@ -134,7 +63,6 @@ const Signup = ({ values, status }) => {
           placeholder="Email"
           onChange={changeHandler}
         />
-         <label htmlFor = "role">  New User Role </label>
         <Field
           component="input"
           type="text"
